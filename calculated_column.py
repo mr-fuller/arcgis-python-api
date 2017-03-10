8984d3763aa3b8e6654b9e18646c0d19bd747d78
mr-fuller <mrfuller460@gmail.com>
2017-03-09 21:02:37 -0500

    use a for loop; reduce length

--- calculated_column.py
+++ calculated_column.py
@@ -10,44 +10,24 @@ fl = arcrest.agol.FeatureLayer(url=[your feature service url])
 fl2 = arcrest.agol.FeatureLayer(url=[your feature service url])
 # set sql statement for query
 sql= '1=1'
-# Query the Table Feature Layer
-resFeats = fl.query(where=sql, out_fields="*")
-#Iterate through features in the table Feature Layer
-for feat in resFeats:
-    # get the value of the first field 
-    field1 = feat.get_value([first field])
-    # replace empty cells with zeros
-    if field1 == None:
-        field1 = 0.0
-    # get the value of the second field 
-    field2 = feat.get_value([second field])
-    # replace empty cell with zero
-    if field2 == None:
-        field2 = 0.0
-    # Calculate the difference in fields
-    fertDiff = field2 - field1
-    # Set the value of the field for difference
-    feat.set_value([difference field], fertDiff)
-# Update features, and print to check success
-print(fl.updateFeature(features=resFeats))
-
-# Query the Feature Class Feature Layer
-resFeats2 = fl2.query(where=sql, out_fields="*")
-#Iterate through features in the table Feature Layer
-for feat in resFeats2:
-    # get the value of the first field 
-    field1 = feat.get_value([first field])
-    # replace empty cells with zeros
-    if field1 == None:
-        field1 = 0.0
-    # get the value of the second field 
-    field2 = feat.get_value([second field])
-    # replace empty cells with zeros
-    if field2 == None:
-        field2 = 0.0
-    # Calculate the difference in fields
-    fertDiff = field2 - field1
-    # Set the value of the field for difference
-    feat.set_value([difference field], fertDiff)
-# Update features, and print to check success
-print(fl2.updateFeature(features=resFeats2))
+for item in [fl,fl2]:
+    # Query the Table Feature Layer
+    resFeats = item.query(where=sql, out_fields="*")
+    #Iterate through features in the table Feature Layer
+    for feat in resFeats:
+        # get the value of the first field 
+        field1 = feat.get_value([first field])
+        # replace empty cells with zeros
+        if field1 == None:
+            field1 = 0.0
+        # get the value of the second field 
+        field2 = feat.get_value([second field])
+        # replace empty cell with zero
+        if field2 == None:
+            field2 = 0.0
+        # Calculate the difference in fields
+        fertDiff = field2 - field1
+        # Set the value of the field for difference
+        feat.set_value([difference field], fertDiff)
+    # Update features, and print to check success
+    print(item.updateFeature(features=resFeats))
